47209f6a4a27374daf61b7708974d37b261b530e
Nasir-NH <nasirjaan2442440@gmail.com>
2025-08-22 08:09:41 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Amk0hAKB#itv67XOvggULJJMX7FwpcBi2lygB1ZQlNQypXHOCfUg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
